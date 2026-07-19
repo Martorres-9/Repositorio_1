42ae7f782fe3387322d03e1f515866d3c7115f77
Martorres-9 <mariatorres21131@gmail.com>
2026-07-19 00:59:31 -0500

    Add files via upload

---
+++ EJERCICIO8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class EJERCICIO8 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String contraseña;
+            
+            do {
+                System.out.print("Ingrese la contrasenia: ");
+                contraseña = scanner.nextLine();
+            } while (!contraseña.equals("java2026"));
+            System.out.println("Acceso concedido.");
+        }
+    }
+}

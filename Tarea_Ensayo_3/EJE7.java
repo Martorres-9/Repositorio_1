305ee9df7f79965a0cd13a8907c27c51fdda59d6
Martorres-9 <mariatorres21131@gmail.com>
2026-07-19 15:41:45 -0500

    Add files via upload

---
+++ Tarea_Ensayo_3/EJE7.java
@@ -0,0 +1,20 @@
+package ENSAYO3;
+/**
+ * @author Maria De Los Angeles Torres Villamar
+ */
+import java.util.Scanner;
+public class EJE7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[4][4];
+        int suma = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("Ingrese valor [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+                suma = suma + matriz[i][j];
+            }
+        }
+        System.out.println("Suma total: " + suma);
+    }
+}

5aa42af955bc81fa2c6d23c7ee9172627cf98b86
Martorres-9 <mariatorres21131@gmail.com>
2026-07-19 22:04:20 -0500

    Add files via upload

---
+++ Tarea_Ensayo_4/EJERCI1.java
@@ -0,0 +1,11 @@
+package ENSAYO4;
+ /** 
+  * @author Maria de los Angeles Torres Villamr
+ */
+public class EJERCI1 {
+    public static void main(String[] args) {
+        System.out.println("Universidad: Universidad Tecnica de Machala");
+        System.out.println("Materia: Programacion");
+        System.out.println("Estudiante: Maria Torres");
+    }
+}

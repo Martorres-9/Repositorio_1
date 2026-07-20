685a703c306a7f351e63534002b27efdfe8dcb90
Martorres-9 <mariatorres21131@gmail.com>
2026-07-19 22:41:56 -0500

    Add files via upload

---
+++ Tarea_Ensayo_4/EJER2.java
@@ -0,0 +1,14 @@
+package ENSAYO4;
+/**
+ * @author Maria De Los Angeles Torres Villamar
+ */
+public class EJER2 {
+    public static void mostrarPares() {
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        mostrarPares();
+    }
+}
